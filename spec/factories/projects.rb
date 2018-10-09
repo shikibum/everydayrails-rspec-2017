498ee75c6f0d424bf87272963eaa1a102ed75574
shikibum <satoshikibu@gmail.com>
2018-10-09 17:15:07 +0900

    ファクトリ内の重複をなくす

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,8 +1,23 @@
 FactoryBot.define do
   factory :project do
-    sequence(:name) { |n| "Project #{n}" }
-    description 'A test project.'
+    sequence(:name) { |n| "Test Project #{n}" }
+    description 'Sample project for testing purposes'
     due_on 1.week.from_now
     association :owner
+
+  # 昨日が締め切のプロジェクト
+  trait :due_yesterday do
+    due_on 1.day.ago
+  end
+
+  # 今日が締切のプロジェクト
+   trait :due_today do
+    due_on Data.current.in_time_zone
+  end
+
+  # 明日が締切のプロジェクト
+   trait :due_tomorrow do
+    due_on 1.day.from_now
+   end
   end
 end
